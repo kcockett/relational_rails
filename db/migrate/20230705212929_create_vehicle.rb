73074f3750534d8b2bd6c038beb25c720c3619e7
Kaina Cockett <kainacockett@gmail.com>
2023-07-05 19:59:10 -0600

    feat: stores index, US1 complete

---
+++ db/migrate/20230705212929_create_vehicle.rb
@@ -0,0 +1,17 @@
+class CreateVehicle < ActiveRecord::Migration[7.0]
+  def change
+    create_table :vehicles do |t|
+      t.integer :model_year
+      t.string :make
+      t.string :model
+      t.integer :mileage
+      t.boolean :repairs_needed
+      t.integer :store_id
+      t.integer :seating
+      t.date :last_service_date
+      t.integer :engine_hours
+
+      t.timestamps
+    end
+  end
+end
--- spec/features/stores/stores_spec.rb
+++ spec/features/stores/stores_spec.rb
@@ -1,7 +1,18 @@
 require "rails_helper"
-RSpec.describe "The stores pages" do
-  it "US1: See the name of each parent record in the system" do
-    store = Store.create!(store_name: "Moab Tour Company", address1: "427 N. Main Street", address2: null, city: "Moab", state: "UT", zip_code: "84532", currently_hiring: false, manager_name: "Brandon Daley", created_at: Time.now, updated_at: Time.now) 
-    visit "/stores/"
+describe "The stores pages" do
+  # US1:  As a visitor
+  # When I visit '/parents'
+  # Then I see the name of each parent record in the system
+  describe "As a visitor" do
+    describe "US1 - When I visit '/parents'" do
+      it "I see the name of each parent record in the system" do
+        store1 = Store.create!(store_name: "Moab Tour Company", address1: "427 N. Main Street", address2: " ", city: "Moab", state: "UT", zip_code: "84532", currently_hiring: false, manager_name: "Brandon Daley", created_at: Time.now, updated_at: Time.now) 
+        store2 = Store.create!(store_name: "Estes Park ATV Rentals", address1: "222 East Elkhorn Ave", address2: " ", city: "Estes Park", state: "CO", zip_code: "80517", currently_hiring: true, manager_name: "Andy Hitch", created_at: Time.now, updated_at: Time.now)
+        
+        visit "/stores"
+        expect(page).to have_content(store1.store_name)
+        expect(page).to have_content(store2.store_name)
+      end
+    end
   end
 end
